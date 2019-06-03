3d0062c9fc5dcccbcb677014167464bd065ddfb5
nblauch <nblauch@icloud.com>
2019-06-03 13:00:22 -0400

    remove local ADMCode

--- ADMCode/snuz/__init__.py
+++
@@ -1,4 +0,0 @@
-from ADMCode.snuz.ppo.run_ppo import run_ppo
-from ADMCode.snuz.ars.ars import run_ars
-from ADMCode.snuz import ars
-from ADMCode.snuz import ppo
--- ADMCode/snuz/ppo/__init__.py
+++
@@ -1,4 +0,0 @@
-from ADMCode.snuz.ppo import models
-from ADMCode.snuz.ppo import agents
-from ADMCode.snuz.ppo import envs
-from ADMCode.snuz.ppo import storage
